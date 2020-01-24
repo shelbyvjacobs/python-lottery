68cdde2a8638610e28d125005d9680d358e93f92
Shelby Jacobs <shelbyvjacobs@gmail.com>
2020-01-24 11:14:02 -0500

    created calculate_winnings method

--- lottery.py
+++ lottery.py
@@ -7,16 +7,20 @@ def collect_player_numbers():
     player_numbers = input("Enter six numbers between 0 and 50, each separated by a space: ")
     player_numbers_list = player_numbers.split(" ")
     player_numbers_set = {int(number) for number in player_numbers_list}
-    print("The numbers you chose were: " + str(player_numbers_set))
     return player_numbers_set
-
-collect_player_numbers()
 
 def generate_lottery_numbers():
     lottery_numbers_set = set()
     while len(lottery_numbers_set) < 6:
         lottery_numbers_set.add(random.randint(0, 50))
-    print("The lottery numbers are: " + str(lottery_numbers_set))
     return lottery_numbers_set
 
-generate_lottery_numbers()
+def calculate_winnings():
+    player_final_numbers = collect_player_numbers()
+    print("The numbers you chose were: " + str(player_final_numbers))
+    lottery_final_numbers = generate_lottery_numbers()
+    print("The lottery numbers are: " + str(lottery_final_numbers))
+    matched_numbers = lottery_final_numbers.intersection(player_final_numbers)
+    print("You won ${}!".format(100 ** len(matched_numbers)))
+
+calculate_winnings()
