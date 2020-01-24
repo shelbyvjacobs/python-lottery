3e106933d118faaf2112f48024d6cb2fe4176cf8
Shelby Jacobs <shelbyvjacobs@gmail.com>
2020-01-24 10:40:43 -0500

    created method collect_player_numbers

--- lottery.py
+++ lottery.py
@@ -2,3 +2,11 @@
 # Generate 6 random numbers for the lottery
 # Compare user numbers to the lottery numbers
 # Calculate winnings based on number of matched numbers
+
+def collect_player_numbers():
+    player_numbers = input("Enter six numbers, each separated by a space: ")
+    player_numbers_list = player_numbers.split(" ")
+    player_numbers_set = {int(number) for number in player_numbers_list}
+    return player_numbers_set
+
+collect_player_numbers()
